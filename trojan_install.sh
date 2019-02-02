fca596b677686348d941b9139d73e9f1b1a68d57
Novice Wu <novice.wu@gmail.com>
2019-02-02 22:41:49 +0800

    Update trojan_install.sh

--- trojan_install.sh
+++ trojan_install.sh
@@ -83,7 +83,7 @@ echo -e "{
        }
 }" > /etc/trojan/config.json
 
-#Create Trojan Service
+# Create Trojan Service
 echo -e "[Unit]
 After=network.target 
 
@@ -94,7 +94,42 @@ Restart=always
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/trojan.service
 
-#Start Trojan Service
+# Start Trojan Service
 systemctl start trojan
-echo -e "Trojan Installation Complete!"
 
+# Set for Auto-Start
+echo -e "[Unit]
+Description=/etc/rc.local Compatibility
+ConditionPathExists=/etc/rc.local
+
+[Service]
+Type=forking
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardOutput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+
+[Install]
+WantedBy=multi-user.target" >/etc/systemd/system/rc-local.service
+
+echo -e "#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will \"exit 0\" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+systemctl start trojan
+exit 0" >/etc/rc.local
+
+chmod +x /etc/rc.local
+systemctl enable rc-local
+systemctl start rc-local.service
+
+echo -e "Trojan Installation Complete!"
